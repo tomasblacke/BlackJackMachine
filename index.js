f7673a9c4c2e94885cf7fd6f2269cd76b3029337
tomasblacke <tomaseblack@outlook.com>
2024-09-03 00:13:40 -0300

    manejo de nuevas funciones y botones

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-let firstCard
-let secondCard
+let firstCard = 10
+let secondCard = 4
+let cards =[]
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
+let newCardEl = document.getElementById("newCard-el")
 
-if(sum < 21){
-    console.log("Throw a new one")
+function startGame(){
+    renderGame()
+}
 
-}else if(sum === 21){
-    console.log("Black jack")
-    hasBlackJack=true
+function renderGame(){
+    cardEl.textContent = firstCard + " - " + secondCard + cards
+    sumEl.textContent = "Total: " + sum
+    if(sum < 21){
+        message ="Throw a new one"
+        
+    }else if(sum === 21){
+        message = "Black jack"
+        hasBlackJack=true
 
-}else {
-    console.log("Lose")
+    }else {
+        message = "Lose"
+        isAlive = false
+    }
+    messageEl.textContent=message
+}
+function newCard(){
+    let card =8
+    sum += card
 
-
+    renderGame()
 }
